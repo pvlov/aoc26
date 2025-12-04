a17abf975fbe7f465b1c8bd60e06037803b74011
pvlov <Daniel.Pavlov@gmx.de>
2025-12-04 11:14:46 +0100

    finish up to day 3

--- day-02/day02.go
+++ day-02/day02.go
@@ -1,4 +1,99 @@
 package day02
 
-func SolvePartOne(input string) {}
-func SolvePartTwo(input string) {}
+import (
+	"fmt"
+	"iter"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type idRange struct {
+	start int
+	end   int
+}
+
+func parseInput(input string) []idRange {
+	input = strings.TrimSpace(input)
+	ranges := strings.Split(input, ",")
+
+	idRanges := make([]idRange, len(ranges))
+
+	for i, r := range ranges {
+		split := strings.Split(r, "-")
+
+		if len(split) != 2 {
+			panic(fmt.Sprintf("Malformed input at range %d: %s", i, r))
+		}
+
+		start, err := strconv.Atoi(split[0])
+		if err != nil {
+			panic(fmt.Sprintf("Malformed input at range %d: %s", i, r))
+		}
+
+		end, err := strconv.Atoi(split[1])
+		if err != nil {
+			panic(fmt.Sprintf("Malformed input at range %d: %s", i, r))
+		}
+
+		idRanges[i].start = start
+		idRanges[i].end = end
+	}
+
+	return idRanges
+}
+
+func allSlicesEqual[E comparable](seq iter.Seq[[]E]) bool {
+	var firstSlice []E
+	foundFirst := false
+
+	for currentSlice := range seq {
+		if !foundFirst {
+			firstSlice = currentSlice
+			foundFirst = true
+			continue
+		}
+
+		if !slices.Equal(firstSlice, currentSlice) {
+			return false
+		}
+	}
+	return true
+}
+
+func SolvePartOne(input string) {
+	result := 0
+	for _, r := range parseInput(input) {
+		for i := r.start; i <= r.end; i++ {
+			str := strconv.Itoa(i)
+
+			middle := len(str) / 2
+
+			if string(str[:middle]) == string(str[middle:]) {
+				result += i
+			}
+		}
+	}
+
+	fmt.Printf("Part 1: %d\n", result)
+}
+
+func SolvePartTwo(input string) {
+	result := 0
+	for _, r := range parseInput(input) {
+		for i := r.start; i <= r.end; i++ {
+			str := strings.Split(strconv.Itoa(i), "")
+
+			for j := 1; j <= len(str)/2; j++ {
+				chunks := slices.Chunk(str, j)
+
+				if allSlicesEqual(chunks) {
+					result += i
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Part 2: %d\n", result)
+}
--- day-03/day03.go
+++ day-03/day03.go
@@ -1,4 +1,59 @@
 package day03
 
-func SolvePartOne(input string) {}
-func SolvePartTwo(input string) {}
+import (
+	"fmt"
+	"strings"
+)
+
+func maximizeNDigits(input string, numDigits int) int {
+	const zeroByte = '0'
+	totalResult := 0
+
+	for line := range strings.SplitSeq(strings.TrimSpace(input), "\n") {
+		L := len(line)
+
+		if L < numDigits {
+			panic(fmt.Sprintf("Skipping line: too short for %d digits\n", numDigits))
+		}
+
+		currentVal := 0
+		searchStart := 0
+
+		for k := range numDigits {
+
+			remainingNeeded := (numDigits - 1) - k
+
+			searchEnd := L - remainingNeeded
+			bestIdx := searchStart
+
+			for i := searchStart; i < searchEnd; i++ {
+				if line[i] == '9' {
+					bestIdx = i
+					break
+				}
+
+				if line[i] > line[bestIdx] {
+					bestIdx = i
+				}
+			}
+
+			digit := int(line[bestIdx] - zeroByte)
+			currentVal = (currentVal * 10) + digit
+			searchStart = bestIdx + 1
+		}
+
+		totalResult += currentVal
+	}
+
+	return totalResult
+}
+
+func SolvePartOne(input string) {
+	result := maximizeNDigits(input, 2)
+	fmt.Printf("Part 1: %d\n", result)
+}
+
+func SolvePartTwo(input string) {
+	result := maximizeNDigits(input, 12)
+	fmt.Printf("Part 2: %d\n", result)
+}
